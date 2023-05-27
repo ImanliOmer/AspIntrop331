04c4bc6df774c2ad855d6d15e8148b07ecd8ca07
ImanliOmer <100156424+ImanliOmer@users.noreply.github.com>
2023-05-27 16:35:38 +0400

    Add files via upload

---
+++ AspFirstTask/AspFirstTask/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Name = "Omar";
+            ViewBag.Surname = "Imanov";
+            ViewBag.Age = 19;
+            ViewData["Country"] = "Azerbaijan";
+            ViewData["Number"] = "+994555355255";
+            return View();
+        }
+    }
+}
